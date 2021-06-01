4cea4622c6de2cd656a05bddb2a79ef44cabf5f8
ZB Tyoma <zb.tyoma@gmail.com>
2021-06-02 00:48:02 +0700

    make it better

---
+++ public/js/chunk-2d0e5e97.f98d80cc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(e,t,n){"use strict";n.r(t);var o=n("7a23"),r={class:"error"},c=Object(o["f"])("h1",null,"Page not found",-1),u=Object(o["e"])("Go to home");function a(e,t,n,a,d,b){var f=Object(o["v"])("router-link");return Object(o["o"])(),Object(o["d"])("div",r,[c,Object(o["f"])(f,{to:"/"},{default:Object(o["A"])((function(){return[u]})),_:1})])}var d={name:"NotFound"};d.render=a;t["default"]=d}}]);
+//# sourceMappingURL=chunk-2d0e5e97.f98d80cc.js.map
